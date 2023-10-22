dd81a2d6b533b22d3cc1f834a4619a27c6883073
k4mien <kontakt.karolkaminski@gmail.com>
2023-10-23 00:43:02 +0200

    error path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const client = require("./bot.js");
-const error = require("./events/client/error.js");
+const error = require("./utils/error.js");
 const handleCommands = require("./handlers/commandHandler");
 const handleEvents = require("./handlers/eventHandler");
 
