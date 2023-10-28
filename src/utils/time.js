c404adbb5324493a9c3d06395d1e511769070a36
k4mien <kontakt.karolkaminski@gmail.com>
2023-10-28 14:27:07 +0200

    added function to format duration time of track

---
+++ src/utils/time.js
@@ -0,0 +1,22 @@
+export function formatMS_HHMMSS(num) {
+    return [86400000, 3600000, 60000, 1000, 1]
+        .reduce((p, c) => {
+            let res = ~~(num / c)
+            num -= res * c
+            return [...p, res]
+        }, [])
+        .map((v, i) =>
+            i <= 1 && v === 0
+                ? undefined
+                : [
+                    i === 4 ? "." : "",
+                    v < 10 ? `0${v}` : v,
+                    [" Days, ", ":", ":", "", ""][i]
+                ].join("")
+        )
+        .filter(Boolean)
+        .slice(0, -1)
+        .join("")
+}
+
+export const delay = async ms => new Promise(r => setTimeout(() => r(true), ms))
