42bcd8496abcf3160236b983711e066aaf54e0ab
k4mien <kontakt.karolkaminski@gmail.com>
2023-10-29 12:51:50 +0100

    added remove command

---
+++ src/commands/remove.js
@@ -0,0 +1,55 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "remove",
+    aliases: ["rm"],
+    description: "Remove provided song from the queue",
+    options: [],
+    inVoiceChannel: true,
+    run: async (client, message) => {
+        if (!message.guildId) return;
+
+        const voiceChannelId = message.member?.voice?.channelId;
+        const player = client.lavalink.getPlayer(message.guildId);
+
+        if (!voiceChannelId) {
+            return message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription("You have to be in a voice channel!"),
+                ],
+            });
+        }
+
+        if (!player)
+            return message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription("Bot is not connected!"),
+                ],
+            });
+
+        if (player?.voiceChannelId !== voiceChannelId) {
+            return message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription("You are in the different voice channel"),
+                ],
+            });
+        }
+
+        if ((!player.playing && !player.paused) || !player.queue.current)
+            return message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription("There is nothing in the queue right now!"),
+                ],
+            });
+
+
+    },
+};
