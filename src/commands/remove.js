58deaeabbff1d941b7b21b24f7f0264d58333a52
k4mien <kontakt.karolkaminski@gmail.com>
2023-10-31 02:16:09 +0100

    fix

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "Remove provided song from the queue",
     options: [],
     inVoiceChannel: true,
-    run: async (client, message) => {
+    run: async (client, message, args) => {
         if (!message.guildId) return;
 
         const voiceChannelId = message.member?.voice?.channelId;
@@ -50,6 +50,46 @@ module.exports = {
                 ],
             });
 
+        if(!args[0]){
+            return message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription("Please enter a song position!"),
+                ],
+            });
+        }
 
+        const position = Number(args[0]);
+
+        if(isNaN(position)){
+            return message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription("Please enter a valid number!"),
+                ],
+            });
+        }
+
+        if(!(position > 0 && position <= player.queue.tracks.length)){
+            return message.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription("There is no song at this position!"),
+                ],
+            });
+        }
+
+        await message.channel.send({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor("Purple")
+                    .setDescription(`\`[#${position}]\` [**${player.queue.tracks[position-1].info.title}**](${player.queue.tracks[position-1].info.uri}) removed from the queue!`),
+            ],
+        });
+
+        await player.queue.splice(position-1, 1)
     },
 };
