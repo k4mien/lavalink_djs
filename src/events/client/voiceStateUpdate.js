a4598a8538798fecad1b314605d59cf583bd2d90
k4mien <kontakt.karolkaminski@gmail.com>
2023-11-09 17:56:33 +0100

    added voicestateupdate when empty channel -> leave

---
+++ src/events/client/voiceStateUpdate.js
@@ -0,0 +1,13 @@
+module.exports = async function (client) {
+    client.on("voiceStateUpdate", (oldState, newState) => {
+        let oldChannel = oldState?.channel
+        let newChannel = newState?.channel
+
+        if (oldChannel?.members.size <= 1) {
+            const player = client.lavalink.getPlayer(oldState.guild.id)
+            setTimeout(() => {
+                player?.destroy("QueueEmpty")
+            }, 300000)
+        }
+    });
+};
