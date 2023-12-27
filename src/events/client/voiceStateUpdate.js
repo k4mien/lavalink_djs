1dec838017abe8ccc6fe8fcadc4d1edaecfaece4
k4mien <kontakt.karolkaminski@gmail.com>
2023-12-27 17:35:42 +0100

    deleted newChannel var

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,11 +1,10 @@
 module.exports = async function (client) {
-  client.on('voiceStateUpdate', (oldState, newState) => {
-    const oldChannel = oldState?.channel
-    const newChannel = newState?.channel
+    client.on('voiceStateUpdate', (oldState, newState) => {
+        const oldChannel = oldState?.channel
 
-    if (oldChannel?.members.size <= 1) {
-      const player = client.lavalink.getPlayer(oldState.guild.id)
-      player?.destroy('QueueEmpty')
-    }
-  })
+        if (oldChannel?.members.size <= 1) {
+            const player = client.lavalink.getPlayer(oldState.guild.id)
+            player?.destroy('QueueEmpty')
+        }
+    })
 }
