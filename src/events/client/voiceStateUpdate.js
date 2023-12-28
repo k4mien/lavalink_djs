94fd2bf0ffd42be89b44127673a2ed1182187854
k4mien <kontakt.karolkaminski@gmail.com>
2023-12-28 12:20:32 +0100

    unused newstate

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,10 +1,10 @@
 module.exports = async function (client) {
-    client.on('voiceStateUpdate', (oldState, newState) => {
-        const oldChannel = oldState?.channel
+  client.on('voiceStateUpdate', (oldState) => {
+    const oldChannel = oldState?.channel
 
-        if (oldChannel?.members.size <= 1) {
-            const player = client.lavalink.getPlayer(oldState.guild.id)
-            player?.destroy('QueueEmpty')
-        }
-    })
+    if (oldChannel?.members.size <= 1) {
+      const player = client.lavalink.getPlayer(oldState.guild.id)
+      player?.destroy('QueueEmpty')
+    }
+  })
 }
