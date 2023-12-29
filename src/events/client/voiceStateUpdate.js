47c3856f9b3ec3d5265f550695f968f93344ca23
k4mien <kontakt.karolkaminski@gmail.com>
2023-12-29 16:28:32 +0100

    check if user left in channel is bot, leave after 5min

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,10 +1,17 @@
-module.exports = async function (client) {
-  client.on('voiceStateUpdate', (oldState) => {
-    const oldChannel = oldState?.channel
+let foo;
 
-    if (oldChannel?.members.size <= 1) {
-      const player = client.lavalink.getPlayer(oldState.guild.id)
-      player?.destroy('QueueEmpty')
-    }
-  })
+module.exports = async function (client) {
+    client.on('voiceStateUpdate', (oldState) => {
+        const oldChannel = oldState?.channel
+
+        if (oldChannel?.members.size === 1) {
+            if (oldChannel?.members.find(member => member.user.bot === true) !== undefined) {
+                foo = setTimeout(() => {
+                    const player = client.lavalink.getPlayer(oldState.guild.id)
+                    player?.destroy('QueueEmpty')
+                }, 300000)
+            }
+        }
+    })
 }
+
