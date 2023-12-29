b1f13998f2491fb749de9eab4e52092191d9c188
k4mien <kontakt.karolkaminski@gmail.com>
2023-12-29 16:37:13 +0100

    standardjs formatting

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,17 +1,22 @@
-let foo;
+let foo
 
 module.exports = async function (client) {
-    client.on('voiceStateUpdate', (oldState) => {
-        const oldChannel = oldState?.channel
+  client.on('voiceStateUpdate', (oldState) => {
+    const oldChannel = oldState?.channel
 
-        if (oldChannel?.members.size === 1) {
-            if (oldChannel?.members.find(member => member.user.bot === true) !== undefined) {
-                foo = setTimeout(() => {
-                    const player = client.lavalink.getPlayer(oldState.guild.id)
-                    player?.destroy('QueueEmpty')
-                }, 300000)
-            }
-        }
-    })
+    if (foo) {
+      clearTimeout(foo)
+      foo = null
+    }
+
+    if (oldChannel?.members.size === 1) {
+      if (oldChannel?.members.find(member => member.user.bot === true) !== undefined) {
+        foo = setTimeout(() => {
+          const player = client.lavalink.getPlayer(oldState.guild.id)
+          player?.destroy('QueueEmpty')
+        }, 10000)
+        console.log('\nteststt\n')
+      }
+    }
+  })
 }
-
