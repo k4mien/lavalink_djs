5b9b08344927a1efea90af970b69cf817acf2822
k4mien <kontakt.karolkaminski@gmail.com>
2024-01-24 15:57:56 +0100

    changed description for 'help' command

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,6 +1,6 @@
 const { ActivityType } = require('discord.js')
 
 module.exports = async function (client) {
-  client.user.setActivity('!help', { type: ActivityType.Custom })
+  client.user.setActivity('-help', { type: ActivityType.Custom })
   await client.lavalink.init({ ...client.user, shards: 'auto' })
 }
